d4786dcd3fc8ec4dc4c83144ad296e8de3b63a56
ickhov <ickhov@ucdavis.edu>
2020-05-06 09:53:59 -0700

    Create React app

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Auth from './pages/Auth'
+
+const App = () => {
+  return (
+    <>
+      <Auth/>
+    </>
+  );
+};
+
+export default App;
---
+++ assets/colors.js
@@ -0,0 +1,9 @@
+const Colors = {
+    primary: '#c9731e',
+    light: '#ffa24e',
+    dark: '#934700',
+    white: '#fff',
+    black: '#000'
+  }
+  
+export default Colors;
